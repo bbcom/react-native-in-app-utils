cb532c456e90576cf27ae44f1d693e28c5e014c0
Brendan Healy <brendan.healy@bodybuilding.com>
2020-07-14 13:22:52 -0600

    Expanded flowtypes and error reporting, fixed some typos.

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
-import { NativeModules } from 'react-native';
+// @flow
+import { NativeModules } from "react-native";
+import { NO_RESPONSE, NO_PRODUCT_ID } from "./error-codes";
 const { InAppUtils } = NativeModules;
+
+export * from "./error-codes";
+
+export type PurchaseData = {
+  transactionDate: number,
+  transactionIdentifier: string,
+  productIdentifier: string,
+  transactionReceipt: string,
+};
+
+export type PriceType = "INTRODUCTORY" | "SUBSCRIPTION" | "";
+
+export type PaymentMode = "FREETRIAL" | "PAYASYOUGO" | "PAYUPFRONT" | "";
+
+export type SubscriptionPeriod = "DAY" | "WEEK" | "MONTH" | "YEAR" | "";
+
+export type Discount = {
+  identifier: string,
+  type: PriceType,
+  numberOfPeriods: string,
+  price: number,
+  localizedPrice: number,
+  paymentMode: PaymentMode,
+  subscriptionPeriod: SubscriptionPeriod,
+};
+
+export type IntroductoryPrice = {
+  identifier: string,
+  type: PriceType,
+  price: string,
+  paymentMode: PaymentMode,
+  numberOfPeriods: string,
+  subscriptionPeriod: {
+    unit: SubscriptionPeriod,
+    numberOfUnits: string
+  }
+};
+
+export type ProductData = {
+  identifier: string,
+  price: number,
+  currencySymbol: string,
+  currencyCode: string,
+  priceString: string,
+  countryCode: string,
+  downloadable: "true" | "false",
+  description: string,
+  title: string,
+  discounts: Array<Discount>,
+  introductoryPrice:  introductoryPrice,
+  subscriptionPeriod: subscriptionPeriod,
+};
+
+export class IOSIAPError extends Error {
+  constructor(code) {
+    super();
+    this.code = code;
+    this.message = code;
+  }
+}
 
 export const loadProducts = productSkus =>
   new Promise((resolve, reject) => {
@@ -16,7 +78,7 @@ export const purchaseProductWithOffer = (
   productSku,
   offerIdentifier,
   { nonce, timestamp, keyIdentifier, signature }
-) =>
+): Promise<PurchaseData> =>
   new Promise((resolve, reject) => {
     InAppUtils.purchaseProductWithOffer(
       productSku,
@@ -29,10 +91,42 @@ export const purchaseProductWithOffer = (
         if (error) {
           reject(error);
         } else if (!response) {
-          reject(new Error("no_response"));
+          reject(new IOSIAPError(NO_RESPONSE));
+        } else if (!response.productIdentifier) {
+          reject(new IOSIAPError(NO_PRODUCT_ID));
         } else {
           resolve(response);
         }
       }
     );
   });
+
+export const purchaseProduct = productSku => {
+  return new Promise((resolve, reject): Promise<PurchaseData> => {
+    InAppUtils.purchaseProduct(productSku, (error, response) => {
+      if (error) {
+        reject(error);
+      } else if (!response) {
+        reject(new IOSIAPError(NO_RESPONSE));
+      } else if (!response.productIdentifier) {
+        reject(new IOSIAPError(NO_PRODUCT_ID));
+      } else {
+        resolve(response);
+      }
+    });
+  });
+};
+
+export const restorePurchases = () => {
+  return new Promise((resolve, reject): Promise<PurchaseData> => {
+    InAppUtils.restorePurchases((error, response) => {
+      if (error) {
+        reject(error);
+      } else if (!response) {
+        reject(new IOSIAPError(NO_RESPONSE));
+      } else {
+        resolve(response);
+      }
+    });
+  });
+};
